ea780378d017805f3a2264b90f932215352dea4c
Nikolas020 <nitolas020@gmail.com>
2024-11-30 15:19:22 -0500

    Add files via upload

---
+++ Taller Herencia/Taller/perro.js
@@ -0,0 +1,17 @@
+class perro {
+
+    hacersonido(a,b){
+        console.log(a,b);
+    }
+
+    hacersonido(a,b,c){
+        console.log(a,b,c);
+    }
+    hacersonido(a,b,c,d){
+        console.log(a,b,c,d)
+    }
+}
+let perro = new perro()
+// perro.hacersonido(1,2)
+// perro.hacersonido(1,5,7);
+perro.hacersonido(6,1,8,9);
